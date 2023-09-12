77f6661ffc3386aac89648abc21785d824b7e2ef
valand310 <122054574+valand310@users.noreply.github.com>
2023-09-12 20:25:24 +0300

    Add files via upload

---
+++ урок 79 JS/main.js
@@ -0,0 +1,13 @@
+// const age = 21
+
+// if(age >= 18) {
+//     console.log(`Вы совершеннолетний`)
+// } else {
+//     console.log(`Вам еще нет 18`)
+// }
+
+let age = 21
+
+age >= 18 ? console.log(`Вы совершеннолетний`) : console.log(`Вам еще нет 18`)
+
+
