cc2a03047cc1d4454ffa17531c091b457819cae1
valand310 <122054574+valand310@users.noreply.github.com>
2023-09-12 20:25:55 +0300

    Add files via upload

---
+++ урок 81 JS/main.js
@@ -0,0 +1,21 @@
+// 1
+
+const numbers = [2, 4, 12, 34, 56, 43, 76, 23, 22, 22, 11]
+
+for( let i = 0; i < numbers.length; i++ ) {
+    if( numbers[i] % 2 == 0) {
+        console.log(numbers[i])
+    }
+}
+
+// 2
+
+console.log(` `)
+
+const colors = ['Красный', 'Оранжевый', 'Желтый', 'Зеленый', 'Голубой', 'Синий', 'Фиолетовый']
+
+for( let k = colors.length - 1; k >= 0; k-- ) {
+    
+        console.log(colors[k])
+    
+}
