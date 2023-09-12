f4aaae4c94ac3e0524f0eae36dc5e9fba3c122ef
valand310 <122054574+valand310@users.noreply.github.com>
2023-09-12 20:25:42 +0300

    Add files via upload

---
+++ урок 80 JS/main.js
@@ -0,0 +1,34 @@
+//  1
+
+for ( let i = 2; i <= 12; i =  i + 2) {
+    console.log(i)
+}
+
+// 2
+
+console.log(` `)
+
+for( let k = 10; k>=1; k -= 1 ) {
+    console.log(k)
+}
+
+// 3
+
+console.log(` `)
+
+let a = 2
+let b = 10
+
+while( a <= 12 ) {
+    console.log(a)
+    a = a + 2
+    
+}
+
+console.log(` `)
+
+while( b >= 1 ) {
+    
+    console.log(b)
+    b = b - 1
+}
